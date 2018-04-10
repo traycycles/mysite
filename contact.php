aa55ecd22745fc0b6529f37b06b461cfbe4feea5
tracy <traymarkthompson@gmail.com>
2018-04-10 13:27:15 -0500

    form created TODO: form validation aws sdk

--- contact.php
+++ contact.php
@@ -6,7 +6,18 @@
  * Time: 10:06 AM
  */
 include 'includes/header.php' ?>
+<section>
+    <h3>Contact Me</h3>
+    <p>I'd love to discuss my current projects and what I am learning about now.  Please feel free to reach out.</p>
+<form action="#" method="post" id="contactForm">
+    Name:<input type="text" name="name"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    Message:<br> <textarea rows="10" cols="100" name="content" form="contactForm"></textarea><br>
+    <input type="submit" value="send">
 
+
+
+</form></section>
 <!---->
 <!--    <section>-->
 <!--        <ul id="">-->
