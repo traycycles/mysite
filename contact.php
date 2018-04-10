71a707bc091550d644c5c6b6d69c67c3c2a61c1d
tracy <traymarkthompson@gmail.com>
2018-04-10 13:05:29 -0500

    created dir for header/footer

--- contact.php
+++ contact.php
@@ -5,3 +5,13 @@
  * Date: 4/10/2018
  * Time: 10:06 AM
  */
+include 'includes/header.php' ?>
+
+<!---->
+<!--    <section>-->
+<!--        <ul id="">-->
+<!--            <li>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </section>-->
+<?php include 'includes/footer.php' ?>
--- index.php
+++ index.php
@@ -5,3 +5,14 @@
  * Date: 4/10/2018
  * Time: 10:05 AM
  */
+include 'includes/header.php'
+
+?>
+<!---->
+<!--    <section>-->
+<!--        <ul id="">-->
+<!--            <li>-->
+<!--            </li>-->
+<!--        </ul>-->
+<!--    </section>-->
+<?php include 'includes/footer.php' ?>
