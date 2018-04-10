55fd69e88f4f9f3404024872ab7f1f3dbe4998ce
tracy <traymarkthompson@gmail.com>
2018-04-10 17:46:35 -0500

    began validation. TODO RegEx

--- contact.php
+++ contact.php
@@ -5,24 +5,52 @@
  * Date: 4/10/2018
  * Time: 10:06 AM
  */
-include 'includes/header.php' ?>
+include 'includes/header.php';
+$name = $email= $content= "";
+$nameErr= $emailErr= $contentErr= "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+    }
+    if (empty($_POST["email"])) {
+        $emailErr = "email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+    }
+    if (empty($_POST["content"])) {
+        $contentErr = "Name is required";
+    } else {
+        $content = test_input($_POST["content"]);
+    }
+}
+
+function test_input($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+?>
+
 <section>
     <h3>Contact Me</h3>
-    <p>I'd love to discuss my current projects and what I am learning about now.  Please feel free to reach out.</p>
-<form action="#" method="post" id="contactForm">
-    Name:<input type="text" name="name"><br><br>
-    Email: <input type="email" name="email"><br><br>
-    Message:<br> <textarea rows="10" cols="100" name="content" form="contactForm"></textarea><br>
-    <input type="submit" value="send">
+    <p>I'd love to discuss my current projects and what I am learning about now.  Please feel free to reach out.<br>
+        <span class="error">* All fields required</span></p>
 
-
-
-</form></section>
-<!---->
-<!--    <section>-->
-<!--        <ul id="">-->
-<!--            <li>-->
-<!--            </li>-->
-<!--        </ul>-->
-<!--    </section>-->
-<?php include 'includes/footer.php' ?>
+    <form id="contactForm" method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>">
+        Name: <input type="text" name="name">
+        <span class="error">*<?= $nameErr ?></span>
+        <br><br>
+        Email: <input type="email" name="email">
+        <span class="error">*<?= $emailErr ?></span>
+        <br><br>
+        Message:<br> <textarea rows="10" cols="100" name="content" form="contactForm"></textarea>
+        <span class="error">*<?= $contentErr ?></span>
+        <br><br>
+        <input type="submit" value="send">
+    </form></section>
+<?php
+include 'includes/footer.php' ?>
